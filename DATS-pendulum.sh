14725720a66f1278b84da005d89fc1a319cd24b0
baimingc <578607986@qq.com>
2020-02-07 19:43:06 -0500

    add files

---
+++ DATS-pendulum.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python mbexp.py -logdir ./log/PETS \
+    -env gym_pendulum \
+    -o exp_cfg.exp_cfg.ntrain_iters 200 \
+    -o exp_cfg.sim_cfg.delay_hor 10\
+    -o ctrl_cfg.prop_cfg.delay_step 10\
+    -ca opt-type CEM \
+    -ca model-type PE \
+    -ca prop-type E
